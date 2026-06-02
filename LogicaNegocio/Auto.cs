a1cff765d4827cecff200c49e455cbc41174d80c
santiUcse <Ucse>
2026-06-02 20:35:52 -0300

    Clases vehiculo y auto correctamente creadas

---
+++ LogicaNegocio/Auto.cs
@@ -0,0 +1,21 @@
+﻿namespace LogicaNegocio;
+
+public class Auto : Vehiculo
+{
+    public bool EsElectrico {get;set;}
+
+    public Auto(string patente, string nombre, decimal precioBasePorHora, bool esElectrico) : base (patente, nombre, precioBasePorHora)
+    {
+        EsElectrico = esElectrico;
+    }
+
+    public override decimal CalcularPrecio()
+    {
+        decimal precio = PrecioBasePorHora;
+        if (EsElectrico)
+        {
+            precio = PrecioBasePorHora*0.90m;
+        }
+        return precio;
+    }
+}
--- LogicaNegocio/Class1.cs
+++ LogicaNegocio/Class1.cs
@@ -1,6 +1,17 @@
 ﻿namespace LogicaNegocio;
 
-public class Class1
+public abstract class Vehiculo
 {
+    public string Patente {get;set;}
+    public string Nombre {get;set;}
+    public decimal PrecioBasePorHora {get;set;}
 
+    public Vehiculo(string patente, string nombre, decimal precioBasePorHora)
+    {
+        Patente = patente;
+        Nombre = nombre;
+        PrecioBasePorHora = precioBasePorHora;
+    }
+
+    public abstract decimal CalcularPrecio();
 }
